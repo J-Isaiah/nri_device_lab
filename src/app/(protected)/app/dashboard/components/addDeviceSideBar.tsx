4292569a78e6a63cf6de0798125d0d0d3fef2b0a
Isaiah Jacobsen <isaiah.jacobsen1@gmail.com>
2025-08-29 17:21:09 -0500

    Feture: added all input fields for the device adding

--- src/app/(protected)/app/dashboard/components/addDeviceSideBar.tsx
+++ src/app/(protected)/app/dashboard/components/addDeviceSideBar.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import FormInput from "./formComponents/formEntry";
-import FormDropDownMenu from "./formComponents/formDropDown";
+import FormInput from "../../../../components/formEntry";
+import FormDropDownMenu from "../../../../components/formDropDown";
 
 export default function AddDeviceSideBar() {
   const [deviceNicName, setDeviceNicName] = useState<string>("");
-  const [deeviceLocation, setDeviceLocation] = useState<string[]>([""]);
+  const [deviceLocation, setDeviceLocation] = useState<string[]>([""]);
+  const [status, setDeviceStatus] = useState<string[]>([""]);
 
   return (
     <div className="w-full h-full">
       <FormInput labelName="NickName" setFormValue={setDeviceNicName} />
       <FormInput labelName="Device Model" setFormValue={setDeviceNicName} />
       <FormDropDownMenu
-        options={["dork"]}
+        options={["UNO", "UNL", "TAMU"]}
         labelName="Location"
         setDropDownValue={setDeviceLocation}
+      />
+      <FormDropDownMenu
+        options={["Active", "Inactive", "Broken", "Fixing"]}
+        labelName="Status"
+        setDropDownValue={setDeviceStatus}
       />
     </div>
   );
